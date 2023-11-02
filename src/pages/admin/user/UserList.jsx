5c1139f0858747226731729f56e9194a40bd1eab
hanasabila <hana46610@gmail.com>
2023-11-02 23:36:24 +0700

    completed user management features

--- src/pages/admin/user/UserList.jsx
+++ src/pages/admin/user/UserList.jsx
@@ -1,10 +1,11 @@
 import DataTable from "react-data-table-component";
 import axios from "axios";
-import { Text, Center, Heading, Box, Avatar, Flex, Button, IconButton } from "@chakra-ui/react";
+import { Text, Heading, Box, Avatar, Flex, Button } from "@chakra-ui/react";
 import { AddIcon } from '@chakra-ui/icons';
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+// cell's components
 const UserList = ({ row }) => (
   <Flex alignItems='center'>
     <Avatar size='sm' src={ row.photo } />
@@ -14,11 +15,29 @@ const UserList = ({ row }) => (
 
 const ActionButton = ({ row }) => (
   <div className="button" justifycontent="center">
-    <Button mr='10px' size='sm' colorScheme="red"> Delete </Button>
-    <Button size='sm' bgColor='orange' color="white"> Edit </Button>
+    <Button mr='10px' size='sm' colorScheme="red"
+      onClick={() => handleDelete(row.id)}
+    > Delete </Button>
+    <Link to={`/a/users/edit/${row.id}`}>
+      <Button size='sm' bgColor='orange' color="white"> Edit </Button>
+    </Link>
   </div>
 );
 
+// delete function
+function handleDelete(id) {
+  const confirmation = window.confirm("Do you want to delete this user? You can't undo this action.");
+  if (confirmation) {
+    axios.delete(`http://localhost:8000/users/${id}`)
+    .then(res => {
+      alert('User deleted.');
+      window.location.reload();
+    })
+    .catch(err => console.log(err));
+  }
+};
+
+// table custom style
 const style = {
   headCells: {
     style: {
@@ -30,6 +49,9 @@ const style = {
 };
 
 function User() {
+
+  const goTo = useNavigate();
+
   const column = [
     {
       name: 'ID',
@@ -39,8 +61,8 @@ function User() {
     },
     {
       name: 'User',
-      cell: row => <UserList row={ row } />,
-      sortable: true
+      cell: row => <Link to={`/a/users/${row.id}`}> <UserList row={ row } /> </Link>,
+      sortable: true,
     },
     {
       name: 'Email',
@@ -57,37 +79,45 @@ function User() {
     },
   ]
 
-  const [records, setRecords] = useState([]);
+  // fetching API
+
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       axios.get(`http://localhost:8000/users`)
       .then(res => {
-        setRecords(res.data)
+        setData(res.data)
       })
       .catch(err => console.log(err))
     }
     fetchData();
   }, []);
 
-
   return (
     <div className="showUser">
       <Box p='32px'>
-      <Flex justifyContent="space-between">
+      <Flex justifyContent="space-between" alignItems='center'>
         <Heading size="lg" mb={"5"}>
           User
         </Heading>
         <Box className="add-button">
-          <Button variant="ghost" bgColor="#FD9F00" color="white" leftIcon={<AddIcon />} size="sm"> Add </Button>
+          <Button
+           variant="ghost"
+           bgColor="#FD9F00"
+           color="white" 
+           leftIcon={<AddIcon />} size="sm"
+           onClick={() => goTo(`/a/add-user`)}
+          >
+          Add
+          </Button>
         </Box>
       </Flex>
       <Box border="1px" borderColor="gray.200" borderRadius="5px">
         <DataTable
           columns={ column }
-          data={ records }
+          data={ data }
           pagination
-          selectableRows
           customStyles={style}
         />
       </Box>
