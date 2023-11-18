5f305e936a91bb52923894b71520f946955b067a
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-11-18 11:28:02 +0800

    Remove Journal menu from sidebar

--- src/pages/user/home/UserDashboard.jsx
+++ src/pages/user/home/UserDashboard.jsx
@@ -1,32 +1,43 @@
 // import LineChart from "@/components/charts/LineAreaChart";
-import { Box, Center, Heading } from "@chakra-ui/react";
+import {Box, Heading, SimpleGrid} from "@chakra-ui/react";
 import FetchLineChart from "./LineChart.component";
 import FetchBarChart from "./BarChart.component";
 import UserAppointmentsTable from "./UserAppointmentsTable";
-import { columnsData } from "./variables/columnsData";
+import {columnsData} from "./variables/columnsData";
 import tableData from "./variables/tableData.json";
 
 function UserDashboard() {
-  return (
-    <Box p={"32px"}>
-      <Center>
-        <Heading size="xl" color="gray.400">
-          Dashboard
-        </Heading>
-      </Center>
-      <div style={{ display: "flex", gap: "20px" }}>
+    return (
+        <Box p={"32px"}>
+                <Heading size="lg">
+                    Dashboard
+                </Heading>
+            {/* <div style={{ display: "flex", gap: "20px" }}>
         <div style={{ width: "500px", height: "250px", margin: "20px" }}>
           <FetchBarChart />
         </div>
         <div style={{ width: "500px", height: "250px", margin: "20px" }}>
           <FetchLineChart />
         </div>
-      </div>
-      <Box>
-        <UserAppointmentsTable columnsData={columnsData} tableData={tableData} />
-      </Box>
-    </Box>
-  );
+      </div> */}
+
+            <SimpleGrid
+                columns={{
+                base: 1,
+                md: 2,
+                xl: 2
+            }}
+                gap="40px"
+                mb="20px">
+                <FetchBarChart/>
+                <FetchLineChart/>
+            </SimpleGrid>
+
+            <Box>
+                <UserAppointmentsTable columnsData={columnsData} tableData={tableData}/>
+            </Box>
+        </Box>
+    );
 }
 
 export default UserDashboard;
