1454b49226dd9cc2022c13881c91255dad81dac5
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-11-20 00:44:56 +0800

    Update UserDashboard

--- src/pages/user/home/UserDashboard.jsx
+++ src/pages/user/home/UserDashboard.jsx
@@ -1,10 +1,9 @@
 // import LineChart from "@/components/charts/LineAreaChart";
-import { Box, Center, Heading } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
 import FetchLineChart from "./LineChart.component";
 import FetchBarChart from "./BarChart.component";
 import UserAppointmentsTable from "./UserAppointmentsTable";
 import { columnsData } from "./variables/columnsData";
-import tableData from "./variables/tableData.json";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -28,19 +27,20 @@ function UserDashboard() {
 
   return (
     <Box p={"32px"}>
-      <Center>
-        <Heading size="xl" color="gray.400">
-          Dashboard
-        </Heading>
-      </Center>
-      <div style={{ display: "flex", gap: "20px" }}>
-        <div style={{ width: "500px", height: "250px", margin: "20px" }}>
-          <FetchBarChart />
-        </div>
-        <div style={{ width: "500px", height: "250px", margin: "20px" }}>
-          <FetchLineChart />
-        </div>
-      </div>
+      <Heading size="xl">
+        Dashboard
+      </Heading>
+      <SimpleGrid
+          columns={{
+          base: 1,
+          md: 2,
+          xl: 2
+      }}
+          gap="40px"
+          mb="20px">
+          <FetchBarChart/>
+          <FetchLineChart/>
+      </SimpleGrid>
       <Box>
         <UserAppointmentsTable columnsData={columnsData} tableData={appointmentData} />
       </Box>
